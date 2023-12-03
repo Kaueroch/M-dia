651029fd2f653668c6d49993ab248f943e0e829f
Kauê Alcantara Rocha <118750523+garotodeprogramacao@users.noreply.github.com>
2023-12-03 20:39:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calcular Média para passar de ano!!");
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("Digite a primeira nota");
+        int n1= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Digite a segunda nota");
+        int n2= Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Digite a terceira nota");
+        int n3= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Digite a quarta nota");
+        int n4= Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("-----------------");    
+       int Média = (n1 + n2 + n3 + n4) /4; 
+        Console.WriteLine("Sua média é: " + Média);
+        
+        if(Média >= 6){
+            Console.WriteLine("Você passou!!!!");
+        }
+        else{
+            Console.WriteLine("Reprovado...");
+        }
+
+
+
+
+
+    }
+
+}
